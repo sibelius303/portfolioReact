8765534f4face0a19b4cd4bbd01951ae390da0f6
sibelius303 <cesarev17@gmail.com>
2022-10-19 04:41:11 +0000

    About me casi listo

--- src/front/js/pages/AboutMe.js
+++ src/front/js/pages/AboutMe.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
 export const AboutMe = () => {
 	const { store, actions } = useContext(Context);
+	const navigate= useNavigate();
 
 	return (
 		<div className="appContainer">
@@ -14,10 +16,14 @@ export const AboutMe = () => {
         	<Box sx={{ bgcolor: 'black', height: '100%', marginTop: "50px", marginBottom:"50px"}}>
 				<div className="container">
 					<div className="picture">
-						div1
+						<div className="foto"></div>
 					</div>
 					<div className="text">
-						div 2
+						<h1 className="nombre">Cesar Eduardo Vallenilla Torrealba</h1>
+						<h1 data-text="Frontend" className="title">Frontend Developer</h1>
+						<p>Soy un entusiasta de las tecnologías aplicadas al desarrollo web, confío en la colaboración entre equipos para lograr un mejor ecosistema de desarrollo y productos tecnologicos de alta calidad</p>
+						<p>Siempre con la vision de ofrecer soluciones agiles y rapidas a las nececidades del cliente</p>
+						<button className="botonSkills" onClick={()=>navigate("/skills")} color="inherit">Skills</button>
 					</div>
 				</div>
 			</Box>
